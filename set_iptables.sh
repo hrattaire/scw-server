8f18bcac9e5129ad4a64d025625928503477d9d8
CI-Hugues.Rattaire <hugues@CI-Hugues-de-MacBook-Pro.local>
2020-06-20 11:07:51 +0800

    Update iptables

--- set_iptables.sh
+++ set_iptables.sh
@@ -6,6 +6,20 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 #iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
+# Open server config
+iptables -A INPUT -i ens2 -m state --state NEW -p udp --dport 1194 -j ACCEPT
+iptables -A INPUT -i tuntcp443 -j ACCEPT
+iptables -A INPUT -i tunudp1194 -j ACCEPT
+
+iptables -A FORWARD -i tuntcp443 -j ACCEPT
+iptables -A FORWARD -i tunudp1194 -j ACCEPT
+
+iptables -A FORWARD -i tunudp1194 -o ens2 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i tuntcp443 -o ens2 -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+iptables -A FORWARD -i ens2 -o  tunudp1194 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i ens2 -o tuntcp443  -m state --state RELATED,ESTABLISHED -j ACCEPT
+
 # Drop all other sources of traffic
 iptables -A INPUT -j DROP
 
