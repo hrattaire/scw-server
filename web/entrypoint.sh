2620b5ca5bf062986ab1017e65944e49e51af1bf
Hugues Rattaire <hugues.rattaire@cloud-interactive.com>
2020-10-20 17:04:02 +0800

    Add install h5ai

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -4,6 +4,8 @@ set -eu
 #### Configuration ####
 nginx_conf_dir=/etc/nginx/conf.d/*.conf
 env_var_list='${WEB_DOMAIN} ${TRANSMISSION_HOST} ${SSL_CERTIF} ${SSL_CERTIF_KEY} ${NGINX_HTPASSWD}'
+h5ai_url="https://release.larsjung.de/h5ai/h5ai-0.29.2.zip"
+downloads_dir="/var/Downloads"
 
 function replace_env_var() {
     envsubst "$1" < $2 > ${2}.temp
@@ -20,6 +22,11 @@ for filename in $nginx_conf_dir; do
     replace_env_var "$env_var_list" $filename
 done
 echo "NGINX configuration files OK"
+
+cd $downloads_dir
+wget -O h5ai.zip $h5ai_url
+unzip h5ai.zip
+echo "h5ai installed"
 
 # ENTRYPOINT
 echo "Starting server"
